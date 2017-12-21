a0d7d1629fcf9533b0f7c4aef028225580685b9f
Adriano di Lauro <adriano.dilauro@boatinternational.com>
2017-12-21 07:00:49 +0100

    Remove tokens from index

--- index.ts
+++ index.ts
@@ -1,3 +1,1 @@
 export { AbTestsModule, AbTestOptions } from './src/app/modules/angular-ab-tests/module';
-export { AB_TESTS_COOKIE_HANDLER_TOKEN, AB_TESTS_CRAWLER_DETECTOR_TOKEN, AB_TESTS_RANDOM_EXTRACTOR_TOKEN } from './src/app/modules/angular-ab-tests/injection-tokens';
-export { AbTestVersionDirective } from './src/app/modules/angular-ab-tests/directive';
